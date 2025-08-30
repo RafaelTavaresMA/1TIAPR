a47523bcbf5fa997dec94575ae2887d526ad9755
RafaelTavaresMA <rafaeltmalmeida@hotmail.com>
2025-08-29 22:57:22 -0300

    tipos

---
+++ tipos.py
@@ -0,0 +1,25 @@
+var = 1
+var2 = 10.4
+var = 15.2
+
+#serve para comentar o codigo isso aqui
+nome = "Cachorro" 
+
+booleano = True
+booleano = False
+
+Lista = {"batata","pao","arroz"}
+Lista2 = {1,2,3,4}
+
+'''
+
+pode escrever aqui como texto
+
+'''
+
+resultado = var + var2
+print(resultado)
+
+print(nome)
+
+
